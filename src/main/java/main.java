1b7396fd02e4cd3eb519f0c5ca9d9664c8061354
Matthew Plummer <matthew.plummer@youview.com>
2016-05-17 17:33:23 +0100

    Refactoring code to different methods and addtional comments.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,24 +5,26 @@ import java.util.Random;
 
 
 public class main {
-    private static String question;
-    private static Random random = new Random();
-
     /*
     Everything gets called through this process when the alarm is triggered at the time set.
     */
+    private static String question;
+    private static int answer;
+    private static Random random = new Random();
+
     public static void main(String[] args) throws IOException
     {
         generateQuestion(1, 200);
     }
 
-    /*
-    This method should read the users answer from the text box and it will be submitted when they press the button to turn off the alarm.
-     */
+
     private static String readInAnswer() throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        return input;
+        /*
+        This method should read the users answer from the text box and it will be submitted when they press the button to turn off the alarm.
+        */
+        BufferedReader takeAnswerProvided = new BufferedReader(new InputStreamReader(System.in));
+        String userAnswer = takeAnswerProvided.readLine();
+        return userAnswer;
     }
     private static void generateQuestion(int min, int max) throws IOException
     {
@@ -40,8 +42,7 @@ public class main {
 
                 if (readInAnswer().equals(Integer.toString(number1 / number2)))
                 {
-                    System.out.print("Correct!");
-                    System.exit(1);
+                    correctAnswer();
                     /*
                     Insert code to turn off the alarm here.
                      */
@@ -49,23 +50,30 @@ public class main {
                 }
                 else
                 {
-                    System.out.print("Incorrect!");
-                    generateQuestion(1, 200);
+                    answer = number1 / number2;
+                    incorrectAnswer();
+                    /*
+                    Generates new question (question select may need altering.)
+                    */
                 }
             }
             else if (number2%number1 == 0) {
                 question = number2 + "/" + number1 + "=";
                 System.out.print(question);
 
-                if (readInAnswer().equals(Integer.toString(number2 / number1))) {
-                    System.out.print("Correct!");
-                    System.exit(1);
+                if (readInAnswer().equals(Integer.toString(number2 / number1)))
+                {
+                    correctAnswer();
                     /*
                     Insert code to turn off the alarm here.
                      */
-                } else {
-                    System.out.print("Incorrect!");
-                    generateQuestion(1, 200);
+                } else
+                {
+                    answer = number2/number1;
+                    incorrectAnswer();
+                    /*
+                    Generates new question (question select may need altering.)
+                     */
                 }
             }
         }
@@ -75,16 +83,18 @@ public class main {
             System.out.print(question);
             if(readInAnswer().equals(Integer.toString(number2 - number1)))
             {
-                System.out.print("Correct!");
-                System.exit(1);
+                correctAnswer();
                 /*
                 Insert code to turn off the alarm here.
                 */
             }
             else
             {
-                System.out.print("Incorrect!");
-                generateQuestion(1, 200);
+                answer = number2 - number1;
+                incorrectAnswer();
+                /*
+                Generates new question (question select may need altering.)
+                 */
             }
         }
         else if(number2<=20)
@@ -93,16 +103,18 @@ public class main {
             System.out.print(question);
             if(readInAnswer().equals(Integer.toString(number2 * number1)))
             {
-                System.out.print("Correct!");
-                System.exit(1);
+                correctAnswer();
                 /*
                 Insert code to turn off the alarm here.
                 */
             }
             else
             {
-                System.out.print("Incorrect!");
-                generateQuestion(1, 200);
+                answer = number2 * number1;
+                incorrectAnswer();
+                /*
+                Generates new question (question select may need altering.)
+                 */
             }
         }
         else if(equationChooser>100 && equationChooser <= 150)
@@ -111,21 +123,42 @@ public class main {
             System.out.print(question);
             if(readInAnswer().equals(Integer.toString(number2 + number1)))
             {
-                System.out.print("Correct!");
-                System.exit(1);
+                correctAnswer();
                 /*
                 Insert code to turn off the alarm here.
                 */
             }
             else
             {
-                System.out.print("Incorrect!");
-                generateQuestion(1, 200);
+                answer = number2 + number1;
+                incorrectAnswer();
+                /*
+                Generates new question (question select may need altering.)
+                 */
             }
         }
         else
         {
             generateQuestion(1, 200);
         }
+    }
+
+    public static void correctAnswer()
+    {
+        System.out.print("Correct!");
+        /*
+        Turn off the alarm at this point.
+         */
+        System.exit(1);
+
+    }
+
+    public static void incorrectAnswer() throws IOException
+    {
+        System.out.println("Incorrect! The answer was " + answer + ". Here's another.");
+        /*
+        Do not turn off the alarm and generate a new question.
+         */
+        generateQuestion(1, 200);
     }
 }
